30e5ffbd37667fe7342c1ee4f85646d2b3e98b3b
zhufucdev <zhufuzhufu1@gmail.com>
2026-07-05 19:57:46 +0800

    bring short provider back

--- App/MemoryShortcutProvider.swift
+++ App/MemoryShortcutProvider.swift
@@ -1,12 +1,12 @@
 import AppIntents
 
-//struct MemoryShortcutProvider: AppShortcutsProvider {
-//    static var appShortcuts: [AppShortcut] {
-//        AppShortcut(
-//            intent: OpenMemoryIntent(),
-//            phrases: ["Open a memory in \(.applicationName)", "Open \(\.$target) in \(.applicationName)"],
-//            shortTitle: "Open a memory",
-//            systemImageName: "brain.filled.head.profile"
-//        )
-//    }
-//}
+struct MemoryShortcutProvider: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: OpenMemoryIntent(),
+            phrases: ["Open a memory in \(.applicationName)", "Open \(\.$target) in \(.applicationName)"],
+            shortTitle: "Open a memory",
+            systemImageName: "brain.filled.head.profile"
+        )
+    }
+}
